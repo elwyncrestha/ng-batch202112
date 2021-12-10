59fcf0ba7020f4e016e3cb703626239076c22c62
Elvin Shrestha <elvin.shrestha@cotiviti.com>
2021-12-10 17:18:50 +0545

    Day 3: Parent-child communication using @Input decorator

--- src/app/modules/home/components/home-layout/home-layout.component.ts
+++ src/app/modules/home/components/home-layout/home-layout.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-layout.component.scss']
 })
 export class HomeLayoutComponent implements OnInit {
+  footerContent = '2021 Angular Training - Cotiviti';
 
   constructor() { }
 
