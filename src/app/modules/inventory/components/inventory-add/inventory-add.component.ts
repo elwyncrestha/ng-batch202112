00cabf585a82ee077af3fd7019db529363295391
Elvin Shrestha <elwyncrestha@gmail.com>
2021-12-15 17:18:49 +0545

    Day 6: Reactive form

--- src/app/modules/inventory/components/inventory-add/inventory-add.component.ts
+++ src/app/modules/inventory/components/inventory-add/inventory-add.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { InventoryService } from '../../services/inventory.service';
 
 @Component({
   selector: 'app-inventory-add',
   templateUrl: './inventory-add.component.html',
-  styleUrls: ['./inventory-add.component.scss']
+  styleUrls: ['./inventory-add.component.scss'],
 })
 export class InventoryAddComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private readonly inventoryService: InventoryService,
+    private readonly router: Router,
+    private readonly fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.buildForm();
   }
 
+  private buildForm(): void {
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.required),
+      quantity: new FormControl(0, [Validators.required, Validators.min(100)]),
+    });
+    this.form = this.fb.group({
+      name: this.fb.control('', Validators.required),
+      quantity: this.fb.control(0, [Validators.required, Validators.min(100)]),
+    });
+  }
+
+  submit(): void {
+    this.inventoryService.save(this.form.value).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/home/inventories/list']);
+    });
+  }
 }
--- src/app/modules/inventory/inventory.module.ts
+++ src/app/modules/inventory/inventory.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { InventoryListComponent } from './components/inventory-list/inventory-list.component';
 import { InventoryAddComponent } from './components/inventory-add/inventory-add.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { InventoryAddComponent } from './components/inventory-add/inventory-add.
   ],
   imports: [
     CommonModule,
-    InventoryRoutingModule
+    InventoryRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class InventoryModule { }
